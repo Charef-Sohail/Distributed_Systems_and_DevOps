3d745eaa07aad8e1203a7aef4b7f0caef548dc5b
Charef Sohail <charefsohayl@gmail.com>
2026-09-24 23:55:28 +0000

    incompatibility between Springdoc and Spring Boot’s HATEOAS classes.

--- Activite_Pratique_N1-Implementation_un_microservice_avec_springboot/src/main/java/com/enset/bankaccountservice/repositories/BankAccountRepository.java
+++ Activite_Pratique_N1-Implementation_un_microservice_avec_springboot/src/main/java/com/enset/bankaccountservice/repositories/BankAccountRepository.java
@@ -2,7 +2,9 @@ package com.enset.bankaccountservice.repositories;
 
 import com.enset.bankaccountservice.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
 
 }
